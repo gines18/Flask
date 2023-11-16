2801abc779009bdd34961543d2a53caedd37e622
gines18 <ruczkowski.m@gmail.com>
2023-11-16 20:01:10 +0000

    add input form with username in login.html

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def home():
 @app.route("/test")
 def test():
     return render_template("new.html")
+
+@app.route("/login", methods= ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)

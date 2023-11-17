c47a3127bb6db7df4a7e653b05de1fa254bbf83a
gines18 <ruczkowski.m@gmail.com>
2023-11-17 18:16:10 +0000

    implement log out button

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", usr=user)
     else:
         return redirect(url_for("login"))
 
